dbc68b8e7fde47f615e5c87a05d137969a141f93
Alexander Hartmann <alex.hart.278@gmail.com>
2022-11-12 22:57:54 +0100

    SetupSpawner, position + speed

--- Assets/Scripts/Monobehaviours/SetupSpawner.cs
+++ Assets/Scripts/Monobehaviours/SetupSpawner.cs
@@ -2,12 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
 
 public class SetupSpawner : MonoBehaviour
 {
     [SerializeField] private GameObject personPrefab;
     [SerializeField] private int gridSize;
     [SerializeField] private int spread;
+    [SerializeField] private Vector2 speedRange = new Vector2(4, 7);
     private BlobAssetStore blob;
 
 
@@ -17,7 +20,35 @@ public class SetupSpawner : MonoBehaviour
         var settings = GameObjectConversionSettings.FromWorld(World.DefaultGameObjectInjectionWorld, blob);
         var entity = GameObjectConversionUtility.ConvertGameObjectHierarchy(personPrefab, settings);
         var entityManager = World.DefaultGameObjectInjectionWorld.EntityManager;
-        entityManager.Instantiate(entity);
+
+
+        for (int x = 0; x < gridSize; x++)
+        {
+            for (int z = 0; z < gridSize; z++)
+            {
+                var instance = entityManager.Instantiate(entity);
+
+                float3 position = new float3(x * spread, 0, z * spread);
+
+                entityManager.SetComponentData(instance, new Translation
+                {
+                    Value = position
+                });
+
+                //entityManager.SetComponentData(instance, new Destination
+                //{
+                //    Value = position
+                //});
+
+                float speed = UnityEngine.Random.Range(speedRange.x, speedRange.y);
+                entityManager.SetComponentData(instance, new MovementSpeed
+                {
+                    Value = speed
+                });
+            }
+        }
+
+        
 
     }
 
