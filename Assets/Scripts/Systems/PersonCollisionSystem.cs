dd19763aeecd84a468c58d30c5c38d8b77240ad3
Alexander Hartmann <alex.hart.278@gmail.com>
2022-11-22 23:35:51 +0100

    ChangeMaterialColor

--- Assets/Scripts/Systems/PersonCollisionSystem.cs
+++ Assets/Scripts/Systems/PersonCollisionSystem.cs
@@ -5,6 +5,7 @@ using Unity.Jobs;
 using Unity.Mathematics;
 using Unity.Physics;
 using Unity.Physics.Systems;
+using Unity.Rendering;
 using Unity.Transforms;
 
 public partial class PersonCollisionSystem : SystemBase
@@ -20,16 +21,49 @@ public partial class PersonCollisionSystem : SystemBase
 
     struct PersonCollisionJob : ITriggerEventsJob
     {
+        [ReadOnly] public ComponentDataFromEntity<PersonTag> PersonGroup;
+        public ComponentDataFromEntity<URPMaterialPropertyBaseColor> ColorGroup;
+        public float Seed;
+
+
         public void Execute(TriggerEvent triggerEvent)
         {
+            bool isEntityAPerson = PersonGroup.HasComponent(triggerEvent.EntityA);
+            bool isEntityBPerson = PersonGroup.HasComponent(triggerEvent.EntityB);
 
+            if (!isEntityAPerson || !isEntityBPerson) { return; }
+
+            var random = new Random((uint)((1 + Seed) + (triggerEvent.BodyIndexA * triggerEvent.BodyIndexB)));
+
+            random = ChangeMaterialColor(random, triggerEvent.EntityA);
+            ChangeMaterialColor(random, triggerEvent.EntityB);
         }
+
+        private Random ChangeMaterialColor(Random random, Entity entity)
+        {
+            if (ColorGroup.HasComponent(entity))
+            {
+                var colorComponent = ColorGroup[entity];
+                colorComponent.Value.x = random.NextFloat(0, 1);
+                colorComponent.Value.y = random.NextFloat(0, 1);
+                colorComponent.Value.z = random.NextFloat(0, 1);
+                ColorGroup[entity] = colorComponent;
+            }
+
+            return random;
+        }
+
     }
+
+
 
     protected override void OnUpdate()
     {
         Dependency = new PersonCollisionJob
         {
+            PersonGroup = GetComponentDataFromEntity<PersonTag>(true),
+            ColorGroup = GetComponentDataFromEntity<URPMaterialPropertyBaseColor>(),
+            Seed = System.DateTimeOffset.Now.Millisecond
 
         }.Schedule(stepPhysicsWorld.Simulation, Dependency);
     }
